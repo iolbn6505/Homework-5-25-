5cfbf7713bfa35c404d0a469679c5278c7833bb9
aiolbn500119 <aiolbn500119@gmail.com>
2018-05-25 01:20:53 +0800

    a non-constant pointer to constant data

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,8 +1,8 @@
-void bubbleSort(int *const element1Ptr,int *const element2Ptr)
+void bubbleSort(const int *element1Ptr,const int *element2Ptr)
 {
 	int hold;
 	hold=*element1Ptr;
-	*element1Ptr= *element2Ptr;
+	*element1Ptr=*element2Ptr;
 	*element2Ptr=hold;
 }
 
